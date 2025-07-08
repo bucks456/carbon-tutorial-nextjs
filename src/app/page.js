3b64ce5ab3c3ae05003e0cf5c289ed3bd0bcf8c3
Alexander Levy <alexlevy@Alexanders-MacBook-Pro.local>
2025-07-08 15:07:20 -0500

    feat(tutorial): complete step 1

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,19 @@
+'use client';
+import { Button } from '@carbon/react';
+import TutorialHeader from '../components/TutorialHeader/TutorialHeader';
+import { Layout } from '@carbon/react/lib/components/Layout';
+import LandingPage from './home/page';
 
-export default function Home() {
-  return (
-    <div>
-Hello Carbon! Well, not quite yet. This is the starting point for the Carbon NextJS tutorial.
-    </div>
-
-  )
+export default function Page() {
+  return <LandingPage />;
 }
+
+// export default function Home() {
+//   return (
+//     // <TutorialHeader></TutorialHeader>
+//     <Layout>
+
+//     </Layout>
+
+//   )
+// }
